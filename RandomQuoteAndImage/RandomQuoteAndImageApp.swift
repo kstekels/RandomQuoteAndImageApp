cbd64de903eadcc3ce32a8d48986ae04e185b666
Karlis <karlisstekels.dev@gmail.com>
2023-02-03 15:36:25 +0200

    Initial Commit

---
+++ RandomQuoteAndImage/RandomQuoteAndImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteAndImageApp.swift
+//  RandomQuoteAndImage
+//
+//  Created by karlis.stekels on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteAndImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
