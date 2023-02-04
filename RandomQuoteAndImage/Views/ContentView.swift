d1c04e02f01cf3e8e34b6972d7ee580f98861f23
Karlis <k.stekels+github@gmail.com>
2023-02-04 10:30:43 +0200

    Added unstructured task using Task

--- RandomQuoteAndImage/Views/ContentView.swift
+++ RandomQuoteAndImage/Views/ContentView.swift
@@ -12,23 +12,36 @@ struct ContentView: View {
     @StateObject private var randomImageListVM = RandomImageListViewModel()
     
     var body: some View {
-        List(randomImageListVM.randomImages) { randomImage in
-            
-            HStack {
-                randomImage.image.map {
-                    Image(uiImage: $0)
-                        .resizable()
-                        .cornerRadius(20)
-                        .frame(width: 100, height: 150)
-                        .aspectRatio(contentMode: .fill)
+        NavigationView {
+            List(randomImageListVM.randomImages) { randomImage in
+                
+                HStack {
+                    randomImage.image.map {
+                        Image(uiImage: $0)
+                            .resizable()
+                            .cornerRadius(20)
+                            .frame(width: 100, height: 150)
+                            .aspectRatio(contentMode: .fill)
+                    }
+                    Text(randomImage.quote)
                 }
-                Text(randomImage.quote)
+                
             }
-            
-        }
-        .listStyle(.plain)
-        .task {
-            await randomImageListVM.getRandomImages(ids: Array(100...120))
+            .listStyle(.plain)
+            .task {
+                await randomImageListVM.getRandomImages(ids: Array(100...120))
+            }
+            .navigationTitle("Random Images/Quotes")
+            .toolbar {
+                Button {
+                    Task {
+                        await randomImageListVM.getRandomImages(ids: Array(100...120))
+                    }
+                } label: {
+                    Image(systemName: "arrow.clockwise.circle")
+                }
+                
+            }
         }
     }
 }
